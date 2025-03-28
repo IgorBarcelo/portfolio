e992897d907c2cd92fd1e9b16561d695bc5bc2e9
IgorBarcelo <114026598+IgorBarcelo@users.noreply.github.com>
2025-03-28 17:33:23 -0300

    feat(backend): api de tednologias e projetos

---
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { TecnologiaPrisma } from './tecnologia.prisma';
+import { Tecnologia } from '@prisma/client';
+
+@Controller('tecnologias')
+export class TecnologiaController {
+    constructor(private readonly repo: TecnologiaPrisma) { }
+
+    @Get()
+    async obterTodas(): Promise<Tecnologia[]> {
+        return this.repo.obterTodas()
+    }
+
+    @Get("destaques")
+    async obterDestaques(): Promise<Tecnologia[]> {
+        return this.repo.obterDestaques()
+    }
+}
