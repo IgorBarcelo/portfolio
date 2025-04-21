b552f67d9559edee63cdb4fac2dfcdb206286448
Igor Barcelo <igor_codmw@hotmail.com>
2025-04-21 20:36:03 -0300

    feat(at env)

--- frontend/src/function/api.ts
+++ frontend/src/function/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "localhost:4000"
 
 export async function httpGet(url: string) {
     const response = await fetch(normalizarUrl(`${API_URL}${url}`))
