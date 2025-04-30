1281f71ba789b4f090f4e251ddb864467ee8b1b2
IgorBarcelo <114026598+IgorBarcelo@users.noreply.github.com>
2025-04-30 19:42:06 -0300

    feat:(at frontend)

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -21,7 +21,7 @@ export default async function PaginaProjeto(props: { params: Promise<{ id: strin
                 before:bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_0%,oklch(0.141_0.005_285.823)_100%)] pt-20
                 ">
             <Cabecalho />
-            <Container className="py-7 flex flex-col items-center gap-10">
+            <Container className="relative py-7 flex flex-col items-center gap-10">
                 <h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
                 <CarroselImagens imagens={projeto.imagens.slice(0)} />
                 <div className="bg-zinc-700/80 p-4 rounded-2xl flex flex-col gap-4">
